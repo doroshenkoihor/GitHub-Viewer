d2b200ca2841fb081d22de52ba30fef98fac474e
Igor <2308igor@mail.ru>
2022-08-15 23:10:06 +0200

    Add VCR gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_07_09_131901) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "spendings", force: :cascade do |t|
-    t.integer "category", null: false
-    t.text "description"
-    t.decimal "amount", precision: 10, scale: 2
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_spendings_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "name"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
--- spec/graphql/queries/user_spec.rb
+++ spec/graphql/queries/user_spec.rb
@@ -2,22 +2,24 @@ require "rails_helper"
 
 RSpec.describe "query user" do
   it "returs info about user and his repos" do
-    query = <<-GQL
-    query {
-      user(login: "doroshenkoihor") {
-        login
-        name
-        repos {
+    VCR.use_cassette("user") do
+      query = <<-GQL
+      query {
+        user(login: "doroshenkoihor") {
+          login
           name
+          repos {
+            name
+          }
         }
       }
-    }
-    GQL
+      GQL
 
-    results = GithubSchema.execute(query)
+      results = GithubSchema.execute(query)
 
-    expect(results.dig('data', 'user', 'login')).to eq('doroshenkoihor')
-    expect(results.dig('data', 'user', 'name')).to eq('doroshenkoihor')
-    expect(results.dig('data', 'user', 'repos')).to eq([{ "name"=>"github" }, { "name"=>"Library" }, { "name"=>"wallet" }])
+      expect(results.dig('data', 'user', 'login')).to eq('doroshenkoihor')
+      expect(results.dig('data', 'user', 'name')).to eq('doroshenkoihor')
+      expect(results.dig('data', 'user', 'repos')).to eq([{ "name"=>"github" }, { "name"=>"Library" }, { "name"=>"wallet" }])
+    end
   end
 end
